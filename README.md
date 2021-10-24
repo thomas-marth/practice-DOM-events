aaf050e6604ee0340d1b68283665880a61ff7824
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2021-10-24 12:54:17 +0300

    upd

---
+++ README.md
@@ -0,0 +1,6 @@
+# Todo
+
+- События. Создание и удаление слушателей
+- Объект события
+- Поведение по умолчанию
+- click, input, submit, keyboard events, mouse events
---
+++ js/03-input.js
@@ -0,0 +1,37 @@
+/*
+ * События input и change
+ * Чекбоксы и свойство checked
+ */
+
+// const refs = {
+//   btn: document.querySelector('.js-button'),
+//   input: document.querySelector('.js-input'),
+//   nameLabel: document.querySelector('.js-button > span'),
+//   license: document.querySelector('.js-license'),
+// };
+
+const btnRef = document.querySelector('.js-button');
+const inputRef = document.querySelector('.js-input');
+const nameLabelRef = document.querySelector('.js-button > span');
+const licenseRef = document.querySelector('.js-license');
+
+inputRef.addEventListener('focus', handleInputFocus);
+inputRef.addEventListener('blur', handleInputBlur);
+inputRef.addEventListener('input', handleInputChange);
+licenseRef.addEventListener('change', handleLicenseChange);
+
+function handleInputFocus(event) {
+  console.log('Получил фокус!');
+}
+
+function handleInputBlur(event) {
+  console.log('Потерял фокус!');
+}
+
+function handleInputChange(event) {
+  nameLabelRef.textContent = event.target.value;
+}
+
+function handleLicenseChange(event) {
+  btnRef.disabled = !event.target.checked;
+}
